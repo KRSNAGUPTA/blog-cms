3d51cb2bacb45e6c7f1b9ec78c1845c2c577d071
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-12-13 21:19:21 +0530

    Implement Express server with routing and middleware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,55 @@
-console.log("hello");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import authMiddleware from "./middlewares/authMiddleware.js";
+import authorizeRole from "./middlewares/roalMiddleware.js";
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+app.get("/api/auth/status", (req, res) => {
+  res.send("API for Auth is running....");
+});
+app.use("/api/auth", authRoutes);
+
+app.get("/api/user/status", (req, res) => {
+  res.send("API for User is running....");
+});
+app.use("/api/user", userRoutes);
+
+app.get(
+  "/api/admin/status",
+  authMiddleware,
+  authorizeRole("admin"),
+  (req, res) => {
+    res.send("API for Admin is running....");
+  }
+);
+app.use("/api/admin", authMiddleware, authorizeRole("admin"), adminRoutes);
+
+app.get("/api/posts/status", (req, res) => {
+  res.send("API for Posts is running....");
+});
+app.use("/api/posts", postRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} \nhttp://localhost:${port}`);
+});
