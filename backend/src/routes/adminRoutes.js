dc6a7754fbf9b2e2cc2a5ccc1c19f4f2c6dc064b
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-12-19 13:24:26 +0530

    Add getWebsiteStats route to adminRoutes with authentication and authorization

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeRole } from "../controllers/adminController.js";
+import { changeRole, getWebsiteStats } from "../controllers/adminController.js";
 import {  upgradeToEditor } from "../controllers/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import authorizeRole from "../middlewares/roalMiddleware.js";
@@ -8,4 +8,5 @@ const router = Router();
 
 router.patch("/updateRole", changeRole);
 router.post("/upgrade", authMiddleware, authorizeRole("admin"), upgradeToEditor);
+router.get("/webStatus", authMiddleware, authorizeRole("admin"), getWebsiteStats);
 export default router;
