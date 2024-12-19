3c54a7e11e84272f01573eb35f38a67254db353c
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-12-19 13:24:15 +0530

    Enhance user model: add totalPosts and lastLogin fields, and ensure password hashing on save

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -2,42 +2,50 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["admin", "editor", "reader"],
-    default: "reader",
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "editor", "reader"],
+      default: "reader",
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://img3.thuthuatphanmem.vn/uploads/2019/10/10/anh-avatar-doremon_033145784.png",
+    },
+    totalPosts: {
+      type: Number,
+      default: 0,
+    },
+    accessToken: {
+      type: String,
+    },
+    lastLogin: { type: Date },
   },
-  avatar: {
-    type: String,
-    default:
-      "https://img3.thuthuatphanmem.vn/uploads/2019/10/10/anh-avatar-doremon_033145784.png",
-  },
-  accessToken: {
-    type: String
-  },
-},{
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   try {
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt); 
-    next(); 
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
   } catch (error) {
     next(error);
   }
 });
 
 userSchema.methods.comparePassword = function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password); 
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 export default mongoose.model("User", userSchema);
