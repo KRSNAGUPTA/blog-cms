b5959ce87dbb43c77857494a69b64bc4b51a9940
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-12-19 13:24:20 +0530

    Enhance user routes: add authentication middleware to getUser endpoint and implement getUserByUsername route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {getUser, reqEditor, } from "../controllers/userController.js"
+import {getUser, getUserByUsername, reqEditor, } from "../controllers/userController.js"
 import authorizeRole from "../middlewares/roalMiddleware.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 const router = Router();
-router.get("/getuser", getUser);
+router.get("/getuser",authMiddleware, getUser);
+router.get("/u/:username", getUserByUsername);
 router.post("/reqtoedit",authMiddleware, reqEditor )
 export default router;
