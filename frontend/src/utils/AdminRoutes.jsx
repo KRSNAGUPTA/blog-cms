e4924968f1962c9461ed7f9d1d09c28b1f7b3f72
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-12-24 11:35:35 +0530

    Remove UserPublicAdminProfile component and update loading indicators in AdminRoute and ProtectedRoute components

--- frontend/src/utils/AdminRoutes.jsx
+++ frontend/src/utils/AdminRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
+import LoadingComponent from "@/components/Loading";
 
 const AdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingComponent/>
   }
 
   if (user.role !== "admin") {
--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
+import LoadingComponent from "@/components/Loading";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
-  // If loading, show loading spinner or placeholder
   if (loading) {
-    return <div>Loading...</div>; // Optional: You can replace this with a spinner or a better UI
+    return <LoadingComponent/> 
   }
 
-  // If no user is available, redirect to login
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  return children; // If user exists, render the children
+  return children;
 };
 
 export default ProtectedRoute;
